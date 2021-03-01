49c5cad2ac5803271f835a167205b2f33f5a6d80
RohitNalePUCSD <rohitnale738@gmail.com>
2021-03-01 18:37:44 +0530

    haml code and destory, show api

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,6 @@
+# ApplicationController
+class ApplicationController < ActionController::Base
+  def default_url_options
+    { locale: I18n.locale }
+  end
+end
---
+++ db/migrate/20210225100651_create_customers.rb
@@ -0,0 +1,11 @@
+class CreateCustomers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :customers do |t|
+      t.string :name
+      t.integer :age
+      t.string :city
+
+      t.timestamps
+    end
+  end
+end
